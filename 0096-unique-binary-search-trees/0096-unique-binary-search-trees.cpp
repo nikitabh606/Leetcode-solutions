f9ae19df06e80f0b61f08af66f490123c3ad6f3c
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-09-11 13:54:51 +0530

    Time: 0 ms (100%), Space: 6.1 MB (14.51%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
+vector<int> memo;
+
+int numUniqueBSTs(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+
+    if (memo[n] != -1) {
+        return memo[n];
+    }
+
+    int count = 0;
+    for (int i = 1; i <= n; ++i) {
+        count += numUniqueBSTs(i - 1) * numUniqueBSTs(n - i);
+    }
+
+    memo[n] = count;
+    return count;
+}
+
+    
     int numTrees(int n) {
-        unsigned long int res = 1;
-        int k=n;
-        int nn=2*n;
-        if (k>nn-k)  k=nn-k;
-        for (int i=0;i<k;i++)
-        {
-            res*=(nn-i);
-            res/=(i+1);
-        }
-        res=res/(n+1);
-        return res;
+        // unsigned long int res = 1;
+        // int nn=2*n;
+        // // if (k>nn-k)  k=nn-k;
+        // for (int i=0;i<n;i++)
+        // {
+        //     res*=(nn-i);
+        //     res/=(i+1);
+        // }
+        // res=res/(n+1);
+        // return res;
+
+        memo.assign(n + 1, -1);
+        return numUniqueBSTs(n);
+
     }
 };
