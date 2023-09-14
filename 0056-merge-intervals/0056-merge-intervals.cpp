cb8274c5b8827e4211b234b02e5b94165e9385a8
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-09-14 06:18:15 +0530

    Time: 37 ms (32.37%), Space: 20.4 MB (10.57%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // priority_queue<pair<int,int>>pq;
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>merged;
+        for(auto it:intervals){
+            if(merged.empty() || merged.back()[1] < it[0])
+                merged.push_back(it);
+            else 
+                merged.back()[1]=max(merged.back()[1],it[1]); 
+        }
+
+        return merged;
+    }
+};
