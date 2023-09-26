11652b22f7f01e34e81aceaedef27aef6140c778
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-09-26 09:23:17 +0530

    Time: 6 ms (44.12%), Space: 8 MB (8.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,14 +1,42 @@
 class Solution {
  public:
-  bool isPalindrome(string s) {
-    for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
-      while (i < j && !isalnum(s[i]))
-        i++;
-      while (i < j && !isalnum(s[j]))  
-        j--; 
-      if (tolower(s[i]) != tolower(s[j]))
-        return false;
+  string convert(string s, int n){
+    string temp;
+    for(int i=0; i<n; i++){
+      if(isalnum(s[i])){
+        temp.push_back(tolower(s[i]));
+      }
     }
-    return true;
+    return temp;
+  }
+
+  bool isPalindrome(string s) {
+    // for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+    //   while (i < j && !isalnum(s[i]))
+    //     i++;
+    //   while (i < j && !isalnum(s[j]))  
+    //     j--; 
+    //   if (tolower(s[i]) != tolower(s[j]))
+    //     return false;
+    // }
+    // return true;
+
+    int n= s.length();
+    string temp= convert(s, n);
+    // cout<<temp;
+    int len= temp.length();
+    if(len==0) return true;
+    int low= 0;
+    int high= len-1;
+    while(low<high){
+      if(temp[low]!=temp[high]){
+        return false;
+      }
+      low++;
+      high--;
+    }
+    return true;;
+
+
   }
 };
