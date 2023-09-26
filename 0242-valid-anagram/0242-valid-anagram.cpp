1b2d374482eec789ca19822f6f7fab300baa8120
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-09-26 09:27:01 +0530

    Time: 10 ms (54.55%), Space: 7.8 MB (6.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -11,13 +11,19 @@ public:
         // return true;
         int n= s.length(), m= t.length();
         if(!(n==m)) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i= 0; i<n; i++){
-            if(s[i]!=t[i]){
-                return false;
-            }
-        }
-        return true;
+
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // for(int i= 0; i<n; i++){
+        //     if(s[i]!=t[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        map<int, int > mpp1, mpp2;
+        for(auto it: s) mpp1[it]++;
+        for(auto it: t) mpp2[it]++;
+        if(mpp1==mpp2) return true;
+        return false;
     }
 };
