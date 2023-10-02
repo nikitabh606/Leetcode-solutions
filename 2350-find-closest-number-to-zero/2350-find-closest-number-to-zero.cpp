57f68b4715d9824e28dc508a3c5854f8bf98f07e
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-10-02 14:56:45 +0530

    Time: 35 ms (5.04%), Space: 27.1 MB (5.04%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cpp
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cpp
@@ -4,9 +4,8 @@ public:
         int mini=INT_MAX;
         unordered_map<int,int>mp;
         for(auto it:nums){
-            int x=abs(it-0);
-            mp[it]=x;
-            mini=min(mini,x);
+            mp[it]=abs(it);
+            mini=min(mini,abs(it));
         }
         for(auto it:mp){
             if(it.second==mini){
