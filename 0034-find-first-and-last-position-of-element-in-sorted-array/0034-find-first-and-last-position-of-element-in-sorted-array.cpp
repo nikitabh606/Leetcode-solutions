9b6979c71a9ded0c7598b5f99337a2b3532c471e
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-10-09 19:56:02 +0530

    Time: 9 ms (19.22%), Space: 29.2 MB (8.57%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    void bin(vector<int> &nums, int l , int h, int t, set<int> &s){
+        if(l>h) return;
+        else
+        {
+            int mid= l+((h-l)/2);
+            if(t==nums[mid])
+            {
+                s.insert(mid);
+                bin(nums,l,mid-1,t,s);
+                bin(nums,mid+1,h,t,s);
+            }
+            else if(t<nums[mid])
+            {
+                bin(nums,l,h-1,t,s);
+            }
+            else
+            {
+                bin(nums,l+1,h,t,s);
+            }
+        }
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        set<int>s; vector<int> v= {-1,-1};
+        bin(nums,0,nums.size()-1,target,s);
+        if(s.size()==1)
+        {
+            v[0]=v[1]= *s.begin();
+        }
+        else if(s.size()>1)
+        {
+            v[0]=*s.begin();
+            v[1]=*s.crbegin();
+        }
+        // for(auto it: s) cout<< it<<" ";
+        return v;
+    }
+};
