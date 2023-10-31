9a85cc5cad5ef6cc8333effd257849ff64f36eca
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-10-31 21:33:43 +0530

    Time: 0 ms (100%), Space: 7.6 MB (43.5%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+
+        int gcd_len = __gcd(len1, len2);
+        return str1.substr(0, gcd_len);
+    }
+};
