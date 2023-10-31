c013fcdeb011d53bd1599e25d126ea937a42c192
Nikita Bhandari <112792554+nikitabh606@users.noreply.github.com>
2023-10-31 12:18:41 +0530

    Time: 30 ms (66.25%), Space: 19.3 MB (65.97%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // priority_queue<pair<int,int>>pq;
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>>merged;
-        for(auto it:intervals){
-            if(merged.empty() || merged.back()[1] < it[0])
-                merged.push_back(it);
-            else 
-                merged.back()[1]=max(merged.back()[1],it[1]); 
+    vector<vector<int>> merge(vector<vector<int>> &intervals) {
+    int n = intervals.size(); // size of the array
+
+    //sort the given intervals:
+    sort(intervals.begin(), intervals.end());
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) { // select an interval:
+        int start = intervals[i][0];
+        int end = intervals[i][1];
+
+        //Skip all the merged intervals:
+        if (!ans.empty() && end <= ans.back()[1]) {
+            continue;
         }
 
-        return merged;
+        //check the rest of the intervals:
+        for (int j = i + 1; j < n; j++) {
+            if (intervals[j][0] <= end) {
+                end = max(end, intervals[j][1]);
+            }
+            else {
+                break;
+            }
+        }
+        ans.push_back({start, end});
     }
+    return ans;
+}
+
+
+    // vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    //     sort(intervals.begin(),intervals.end());
+    //     vector<vector<int>>arr;
+    //     for(auto it:intervals){
+    //         if(arr.empty() || arr.back()[1] < it[0])
+    //             arr.push_back(it);
+    //         else 
+    //             arr.back()[1]=max(arr.back()[1],it[1]); 
+    //     }
+
+    //     return arr;
+    // }
 };
